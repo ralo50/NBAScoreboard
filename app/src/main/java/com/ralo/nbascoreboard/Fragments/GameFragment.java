99b8ddf06bbb3b4f25677b6551f9bd85a56a76e4
ralo50 <ekimekim1>
2018-11-17 20:43:36 +0100

    Testing layout

--- app/src/main/java/com/ralo/nbascoreboard/Fragments/GameFragment.java
+++ app/src/main/java/com/ralo/nbascoreboard/Fragments/GameFragment.java
@@ -13,7 +13,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
