d75957e545c696f67fad076f4a65e3b0c9e17e9d
Luka <ralo.luka@gmail.com>
2018-11-17 16:46:06 +0100

    Added fragments

---
+++ app/src/main/java/com/ralo/nbascoreboard/Fragments/MatchupFragment.java
@@ -0,0 +1,31 @@
+package com.ralo.nbascoreboard.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ralo.nbascoreboard.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MatchupFragment extends Fragment {
+
+
+    public MatchupFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_matchup, container, false);
+    }
+
+}
