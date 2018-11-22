db3a4e2a2f865a2c10bbb70af577a6c88439ab1d
Luka <ralo.luka@gmail.com>
2018-11-22 19:14:32 +0100

    All the teams' data

--- app/src/main/java/com/ralo/nbascoreboard/Utils/JsonTeamParser.java
+++ app/src/main/java/com/ralo/nbascoreboard/Utils/JsonTeamParser.java
@@ -94,9 +94,9 @@ public class    JsonTeamParser {
 
     public int getAwayTeamFieldGoalsMade(){
         try {
-            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
             newJasonObject = newJasonObject.getJSONObject("stats");
-            return newJasonObject.getInt("points");
+            return newJasonObject.getInt("field_goals_made");
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -104,5 +104,527 @@ public class    JsonTeamParser {
         return 0;
     }
 
+    public int getHomeTeamFieldGoalsMade(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("field_goals_made");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getAwayTeamFieldGoalsAttempted(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("field_goals_attempted");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamFieldGoalsAttempted(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("field_goals_attempted");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public float getAwayTeamFieldGoalsPercentage(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return (float) newJasonObject.getDouble("field_goals_percentage");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public float getHomeTeamFieldGoalsPercentage(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return (float) newJasonObject.getDouble("field_goals_percentage");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getAwayTeamFreeThrowsMade(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("free_throws_made");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamFreeThrowsMade(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("free_throws_made");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public int getAwayTeamFreeThrowsAttempted(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("free_throws_attempted");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamFreeThrowsAttempted(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("free_throws_attempted");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public float getAwayTeamFreeThrowsPercentage(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return (float) newJasonObject.getDouble("free_throws_percentage");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public float getHomeTeamFreeThrowsPercentage(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return (float) newJasonObject.getDouble("free_throws_percentage");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public int getAwayTeamThreePointersMade(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("three_pointers_made");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamThreePointersMade(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("three_pointers_made");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public int getAwayTeamThreePointersAttempted(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("three_pointers_attempted");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamThreePointersAttempted(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("three_pointers_attempted");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public float getAwayTeamThreePointersPercentage(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return (float) newJasonObject.getDouble("three_pointers_percentage");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public float getHomeTeamThreePointersPercentage(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return (float) newJasonObject.getDouble("three_pointers_percentage");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getAwayTeamOffensiveRebounds(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("rebounds_offensive");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamOffensiveRebounds(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("rebounds_offensive");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public int getAwayTeamDefensiveRebounds(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("rebounds_defensive");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamDefensiveRebounds(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("rebounds_defensive");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public int getAwayTeamTeamRebounds(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("team_rebounds");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamTeamRebounds(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("team_rebounds");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getAwayTeamAssists(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("assists");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeAssists(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("assists");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public int getAwayTeamFouls(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("fouls");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamFouls(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("fouls");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getAwayTeamTeamFouls(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("team_fouls");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamTeamFouls(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("team_fouls");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getAwayTeamTechnicalFouls(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("technical_fouls");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamTechnicalFouls(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("technical_fouls");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getAwayTeamSteals(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("steals");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamSteals(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("steals");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getAwayTeamTurnovers(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("turnovers");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamTurnovers(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("turnovers");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getAwayTeamTeamTurnovers(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("team_turnovers");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamTeamTurnovers(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("team_turnovers");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getAwayTeamBlocks(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("blocks");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamBlocks(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("blocks");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getAwayTeamShortTimeoutsRemaining(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("short_timeout_remaining");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamShortTimeoutsRemaining(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("short_timeout_remaining");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public int getAwayTeamFullTimeoutsRemaining(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("visitor");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("full_timeout_remaining");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int getHomeTeamFullTimeoutsRemaining(){
+        try {
+            newJasonObject = this.getBaseJsonObject().getJSONObject("home");
+            newJasonObject = newJasonObject.getJSONObject("stats");
+            return newJasonObject.getInt("full_timeout_remaining");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 
 }
