b23992bd916948bd67b3d37e1808ae3b013d9169
Luka <ralo.luka@gmail.com>
2019-01-15 21:32:20 +0100

    game details preparation

--- app/src/main/java/com/ralo/nbascoreboard/Activities/TeamActivity.java
+++ app/src/main/java/com/ralo/nbascoreboard/Activities/TeamActivity.java
@@ -2,7 +2,6 @@ package com.ralo.nbascoreboard.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ImageView;
 
 import com.ralo.nbascoreboard.R;
@@ -12,6 +11,7 @@ public class TeamActivity extends BaseActivity {
     private int awayTeamLogoImageView;
     private int homeTeamLogoImageView;
     private int teamId;
+    private String url;
 
     private Intent intent;
 
@@ -22,6 +22,13 @@ public class TeamActivity extends BaseActivity {
         setupViews();
     }
 
+    private void getIntents() {
+        intent = getIntent();
+        teamId = intent.getIntExtra("teamId", 0);
+        awayTeamLogoImageView = intent.getIntExtra("awayTeamLogoImageView", 0);
+        homeTeamLogoImageView = intent.getIntExtra("homeTeamLogoImageView", 0);
+    }
+
     private void setupViews() {
         if (intent.getBooleanExtra("isHome", false)) {
             imageView = findViewById(R.id.teamLogoHome);
@@ -30,12 +37,6 @@ public class TeamActivity extends BaseActivity {
             imageView = findViewById(R.id.teamLogoAway);
             imageView.setImageResource(awayTeamLogoImageView);
         }
-    }
-
-    private void getIntents() {
-        intent = getIntent();
-        teamId = intent.getIntExtra("teamId", 0);
-        awayTeamLogoImageView = intent.getIntExtra("awayTeamLogoImageView", 0);
-        homeTeamLogoImageView = intent.getIntExtra("homeTeamLogoImageView", 0);
+        url = "https://stats.nba.com/stats/teamyearbyyearstats/?teamId=" + teamId + "&leagueId=00&seasonType=Regular+Season&perMode=PerGame";
     }
 }
